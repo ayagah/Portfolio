5b02d51f5367c3255843db7aaeb90b171c4ca767
ayaga <ayagajack@gmail.com>
2025-05-22 10:22:11 +0300

    third commit

--- Portflio_Project/settings.py
+++ Portflio_Project/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'Portflio_Project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/tmp/db.sqlite3',  # Vercel requires /tmp
     }
 }
 
